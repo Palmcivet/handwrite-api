fa59d90b6e7952bdab9814caf07e5fb29d4afd3a
Palm Civet <2143899558@qq.com>
2019-05-26 23:23:49 +0800

    1.4.1

--- car/mentally_car/mentally_car.ino
+++ car/mentally_car/mentally_car.ino
@@ -5,22 +5,25 @@
  * Comment:
  */
 #include <Servo.h>
+#include <Stepper.h>
 #define black 1 // HIGH
 #define white 0 // LOW
 
 Servo myservo;
 
-//const int stepper_1 = 4;
-//const int stepper_2 = 5;
+// signal of steeper, use pwm.
+const int port_stepper = 6;
+int val_stepper;
 
 // signal of servo.
-const int servo = 6;
+const int port_servo = 7;
+int val_servo;
 
 // define the digital port of sensor and the value of port.
-const int sensor_R1_D = 8;
-const int sensor_R2_D = 9;
-const int sensor_L2_D = 10;
-const int sensor_L1_D = 11;
+const int port_R1_D = 8;
+const int port_R2_D = 9;
+const int port_L2_D = 10;
+const int port_L1_D = 11;
 
 int val_L1_D;
 int val_L2_D;
@@ -29,10 +32,10 @@ int val_R1_D;
 
 // TODO below ===================
 // define the analog port of sensor and the value of port.
-const int sensor_L1_A = A0;
-const int sensor_L2_A = A1;
-const int sensor_R2_A = A2;
-const int sensor_R1_A = A3;
+const int port_L1_A = A0;
+const int port_L2_A = A1;
+const int port_R2_A = A2;
+const int port_R1_A = A3;
 
 int val_L1_A;
 int val_L2_A;
@@ -82,46 +85,44 @@ void Stop()
 {
 	angle_chge = 90;
 	myservo.write(angle_chge);
-	analogWrite(servo, 0);
+	analogWrite(val_servo, 0);
 	Serial.println("Stop");
 }
 
 void setup()
 {
-	//pinMode(12, OUTPUT);
-	//digitalWrite(12, HIGH);
 	//digitalWrite(stepper_1, HIGH);
 	//digitalWrite(stepper_2, LOW);
-	pinMode(servo, OUTPUT);
-	myservo.attach(servo);
-	
+	pinMode(port_servo, OUTPUT);
+	myservo.attach(port_servo);
+
 	strT();
 
-	pinMode(sensor_L1_D, INPUT);
-	pinMode(sensor_L2_D, INPUT);
-	pinMode(sensor_R2_D, INPUT);
-	pinMode(sensor_R1_D, INPUT);
+	pinMode(port_L1_D, INPUT);
+	pinMode(port_L2_D, INPUT);
+	pinMode(port_R2_D, INPUT);
+	pinMode(port_R1_D, INPUT);
 
 	// TODO
-	pinMode(sensor_L1_A, INPUT);
-	pinMode(sensor_L2_A, INPUT);
-	pinMode(sensor_R2_A, INPUT);
-	pinMode(sensor_R1_A, INPUT);
+	pinMode(port_L1_A, INPUT);
+	pinMode(port_L2_A, INPUT);
+	pinMode(port_R2_A, INPUT);
+	pinMode(port_R1_A, INPUT);
 	Serial.begin(9600);
 }
 
 void loop()
 {
-	val_L1_D = digitalRead(sensor_L1_D);
-	val_L2_D = digitalRead(sensor_L2_D);
-	val_R2_D = digitalRead(sensor_R2_D);
-	val_R1_D = digitalRead(sensor_R1_D);
+	val_L1_D = digitalRead(port_L1_D);
+	val_L2_D = digitalRead(port_L2_D);
+	val_R2_D = digitalRead(port_R2_D);
+	val_R1_D = digitalRead(port_R1_D);
 
 	// TODO
-	val_L1_A = digitalRead(sensor_L1_A);
-	val_L2_A = digitalRead(sensor_L2_A);
-	val_R2_A = digitalRead(sensor_R2_A);
-	val_R1_A = digitalRead(sensor_R1_A);
+	val_L1_A = digitalRead(port_L1_A);
+	val_L2_A = digitalRead(port_L2_A);
+	val_R2_A = digitalRead(port_R2_A);
+	val_R1_A = digitalRead(port_R1_A);
 
 	/* TCRT5000 红外对管传感器参数
      * 检测距离：1~25 mm
