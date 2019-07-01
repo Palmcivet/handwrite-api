b99f35dabc96fb351442a669a866e5ff49b5d416
Palm Civet <2143899558@qq.com>
2019-07-01 09:05:02 +0800

    froze and fork

--- Course/graph.c
+++ Course/graph.c
@@ -1,196 +1,286 @@
-#include <stdio.h>
 /**
- * Project:16
+ * Project: 16
  * Author: Palm Civet
  * Date: 2019-6-28
+ * Feature: 
+ *      1. draw a line with y = ax + b
+ *      2. draw a graph with coordinates.
+ * Github: https://github.com/Palmcivet/Demo/blob/master/Course
  */
 
-#define COLUMN 68 // X-axis
-#define ROW 36    // Y-axis
-#define MAX_PARAMETERS 5
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define COLUMN 68 // X-axis
+#define ROW 36	// Y-axis
 #define X_AXIS 34
 #define Y_AXIS 18
 
+#define MAX_PARAMETERS 20
+#define HELP 0
+#define OPERATION 1
+#define ADD 2
+
 char canvas[ROW][COLUMN];
-char parameters[MAX_PARAMETERS * 2];
-
-int main()
-{
-    help_print();
-    canvas_init();
-
-    switch (parameters[0])
-    {
-    case '0':
-        return 0;
-    case '1':
-        if (!parameters_check(parameters, 2))
-        {
-            for (int y = -Y_AXIS; y <= Y_AXIS; y++)
-            {
-                for (int x = -X_AXIS; x <= X_AXIS; x++)
-                {
-                    if (y == (int)(parameters[2] - '0') * x + (int)(parameters[4] - '0'))
-                    {
-                        //printf("%d %d\n", x, y);
-                        //canvas_draw(y - Y_AXIS, X_AXIS + x);
-                        canvas_draw(Y_AXIS - x, X_AXIS + y);
-                    }
-                }
-            }
-        }
-        break;
-    case '2':
-        if (!parameters_check(parameters, 2))
-        {
-            int a = (int)(parameters[2] - '0');
-            int b = (int)(parameters[4] - '0');
-            // blow are for a don't exist
-            for (int y = -Y_AXIS; y <= Y_AXIS; y++)
-            {
-                for (int x = -X_AXIS; x <= X_AXIS; x++)
-                {
-                    if (b == x)
-                    {
-                        printf("%d %d\n", y, x); //TODO: cancel comment
-                        canvas_draw(Y_AXIS - y, X_AXIS + b);
-                    }
-                }
-            }
-        }
-        break;
-    case '3':
-        if (!parameters_check(parameters, 3))
-            break;
-    case '4':
-        if (!parameters_check(parameters, 2))
-            break;
-    case '5':
-        if (!parameters_check(parameters, 3))
-            break;
-    default:
-        printf("Invalid Parameter: Unselected Graph\n\n");
-        break;
-    }
-    show();
-    //help_print(); //TODO: cancel comment
-}
-
-int parameters_check(int *para, int para_num) //!!! Can't Modify
-{
-    int i = 1;
-    while (i <= para_num)
-    {
-        if (*(parameters + 2 * i) == '\0')
-        {
-            printf("Invalid Parameter: Missing Parameters\n\n");
-            return 1;
-        }
-        else if (*(parameters + 2 * i) <= '0')
-        {
-            printf("Invalid Parameter: Unvalidated Parameter\n\n");
-            return 1;
-        }
-        i++;
-    }
-    return 0;
-}
-
-int help_print()
-{
-    printf("========================================\n");
-    printf("0) Exit\n");
-    printf("1) Line:\tm\tn\n");
-    printf("2) Oval:\ta\tb\n");
-    printf("3) Triangle:\ta\tb\tc\n");
-    printf("4) Rectangle:\ta\tb\n");
-    printf("5) Trapezoid:\ta\tb\th\n");
-    printf("\n");
-    printf("Example:\n");
-    printf("Graph>> 5 0,5 0,8 4,5 2,8\n");
-    printf("========================================\n");
-    printf("\n");
-    printf("Graph>> ");
-    gets(parameters);
-    return 0;
-}
+char parameters[MAX_PARAMETERS * 2] = {0};
 
 int canvas_init()
 {
-    for (int x = 0; x < ROW; x++)
-    {
-        for (int y = 1; y < COLUMN; y++)
-        {
-            if ((x == Y_AXIS) || (y == X_AXIS))
-            {
-                canvas[x][y] = '.';
-            }
-            else
-            {
-                canvas[x][y] = ' ';
-            }
-        }
-    }
-    return 0;
+	for (int y = 0; y <= ROW; y++)
+	{
+		for (int x = 0; x < COLUMN; x++)
+		{
+			if ((y == Y_AXIS) || (x == X_AXIS))
+			{
+				canvas[y][x] = '.';
+			}
+			else
+			{
+				canvas[y][x] = ' ';
+			}
+		}
+	}
+	return 0;
 }
 
 int canvas_draw(int x, int y)
 {
-    if (canvas[x][y] == ' ')
-    {
-        canvas[x][y] = '.';
-    }
-    return 0;
+	if (canvas[x][y] == ' ')
+	{
+		canvas[x][y] = '.';
+	}
+	return 0;
 }
 
 int canvas_print()
 {
-    for (int i = 0; i < ROW; i++) // Y-axis
-    {
-        for (int j = 0; j <= COLUMN * 2; j += 2) // X-axis
-        {
-            if (j == 0 || j == COLUMN * 2)
-            {
-                printf(" |");
-            }
-            else if ((i == Y_AXIS + 1) && (j == COLUMN + 2))
-            {
-                printf(" o");
-            }
-            else
-            {
-                printf(" %c", canvas[i][j / 2]);
-            }
-        }
-        printf("\n");
-    }
-    return 0;
+	// Top border
+	printf(" +");
+	for (int i = 1; i < COLUMN; i++)
+	{
+		printf(" ─");
+	}
+	printf(" +");
+	printf("\n");
+
+	// Canvas
+	for (int i = 0; i <= ROW; i++) // Y-axis
+	{
+		for (int j = 0; j <= COLUMN * 2; j += 2) // X-axis
+		{
+			if (j == 0 || j == COLUMN * 2)
+			{
+				printf(" |");
+			}
+			else if ((i == Y_AXIS + 1) && (j == COLUMN + 2))
+			{
+				printf(" o");
+			}
+			else
+			{
+				printf(" %c", canvas[i][j / 2]);
+			}
+		}
+		printf("\n");
+	}
+
+	// Buttom border
+	printf(" +");
+	for (int i = 1; i < COLUMN; i++)
+	{
+		printf(" ─");
+	}
+	printf(" +");
+	printf("\n");
+	return 0;
 }
 
-int border_print()
+int wizard(int flag, char *args)
 {
-    printf(" +");
-    for (int i = 1; i < COLUMN; i++)
-    {
-        printf(" ─");
-    }
-    printf(" +");
-    printf("\n");
-    return 0;
+	if (flag == HELP)
+	{
+		printf("Example:\n");
+		printf("-------------- Operation ---------------\n");
+		printf("0) Exit\n");
+		printf("1) Help\n");
+		printf("2) Add\n");
+		printf("3) Clean\n");
+		printf("\n");
+		printf("Select an operation:\n");
+		printf("Graph>> a\n");
+		printf("0) Line\n");
+		printf("1) Coordinate\n");
+		printf("\n");
+		printf("Input type and parameters:\n");
+		printf("Graph>> l x1,y1 x2,y2 x3,y3 ...\n");
+		printf("---------------------------------------------------------------------\n");
+		printf("\n");
+		printf("Now select an operation:\n");
+		printf("Graph>> ");
+		scanf("%s", args);
+	}
+	else if (flag == OPERATION)
+	{
+		printf("\n");
+		printf("=============== Operation ==============\n");
+		printf("0) Exit\n");
+		printf("1) Help\n");
+		printf("2) Add\n");
+		printf("3) Clean\n");
+		printf("=====================================================================\n");
+		printf("\n");
+		printf("Select an operation:\n");
+		printf("Graph>> ");
+		scanf("%s", args);
+	}
+	else if (flag == ADD)
+	{
+		printf("=============== Arguments ==============\n");
+		printf("0) Line\n");
+		printf("1) Coordinate\n");
+		printf("=====================================================================\n");
+		printf("\n");
+		printf("Input type and parameters:\n");
+		printf("c 1,2 5,8 4,1\n");
+		printf("Graph>> ");
+		getchar();
+		fgets(args, MAX_PARAMETERS, stdin);
+	}
+	return 0;
 }
 
-int show()
+int gen_line(int a, int b)
 {
-    border_print();
-    canvas_print();
-    border_print();
-    return 0;
+	for (int y = -Y_AXIS; y <= Y_AXIS; y++)
+	{
+		for (int x = -X_AXIS; x <= X_AXIS; x++)
+		{
+			if (y == a * x + b)
+			{
+				// Can't Modified!!!
+				canvas_draw(Y_AXIS - y, X_AXIS + x);
+			}
+		}
+	}
+	return 0;
 }
 
-/**
- * Test examples:
- * Name Coordinate Parameters
- * 矩形 (1,1)(9,9)(1，9)（9，1） 
- * 直线、椭圆形、三角形、矩形、梯形
- */
+int gen_segment(int x_1, int y_1, int x_2, int y_2)
+{
+	// process the raw data
+	// aim: x_1 < x_2 and y_1<y_2
+	printf("before: %d %d %d %d\n", x_1, y_1, x_2, y_2); //TODO: cancel comment
+	if ((x_1 > x_2) || (y_1 > y_2))
+	{
+		int temp = 0;
+		// change x
+		temp = x_2;
+		x_2 = x_1;
+		x_1 = temp;
+		// change y
+		temp = y_2;
+		y_2 = y_1;
+		y_1 = temp;
+		printf("after: %d %d %d %d\n", x_1, y_1, x_2, y_2); //TODO: cancel comment
+	}
+
+	// a don't exist
+	if (x_1 == x_2)
+	{
+		// blow are for the condition that a don't exist
+		for (int y = y_1; y <= y_2; y++)
+		{
+			printf("y: %d x: %d\n", y, x_1); //TODO: cancel comment
+			canvas_draw(Y_AXIS - y, X_AXIS + x_1);
+		}
+	}
+	else
+	{
+		float a = (float)(y_2 - y_1) / (x_2 - x_1);
+		float b = (float)(-x_1) * (y_2 - y_1) / (x_2 - x_1) + y_1;
+		printf("a: %f b: %f\n", a, b); //TODO: cancel comment
+		for (int y = y_1; y <= y_2; y++)
+		{
+			for (int x = x_1; x <= x_2; x++)
+			{
+				if (y == a * x + b)
+				{
+					//printf("Enter\n"); //TODO: cancel comment
+					printf("y: %d x: %d\n", y, x); //TODO: cancel comment
+					//canvas_draw(Y_AXIS - y + 1, X_AXIS + x - 1);
+					canvas_draw(Y_AXIS - y, X_AXIS + x);
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+// Core code
+int parameters_handle(char *parameters)
+{
+	while (strcmp(parameters, "exit") && strcmp(parameters, "quit") && strcmp(&parameters[0], "e") && strcmp(&parameters[0], "q"))
+	{
+		if (!strcmp(parameters, "help") || !strcmp(&parameters[0], "h"))
+		{
+			wizard(HELP, parameters);
+		}
+		else if (!strcmp(parameters, "clean") || !strcmp(&parameters[0], "c"))
+		{
+			canvas_init();
+			printf("Graph cleaned\n");
+			wizard(OPERATION, parameters);
+		}
+		else if (!strcmp(parameters, "add") || !strcmp(&parameters[0], "a"))
+		{
+			wizard(ADD, parameters);
+			if (parameters[0] == 'l') // line
+			{
+				if ((parameters[2] != '\0') && (parameters[4] != '\0'))
+				{
+					gen_line((parameters[2] - '0'), (parameters[4] - '0'));
+				}
+				else
+				{
+					printf("Invalid Parameter: Missing Arguments!\n\n");
+				}
+			}
+			else if (parameters[0] == 'c') // coordinate
+			{
+				int i = 4;
+				while (parameters[2 * i] != '\0')
+				{
+					gen_segment(parameters[2 * i - 6] - '0', parameters[2 * i - 4] - '0', parameters[2 * i - 2] - '0', parameters[2 * i] - '0');
+					//printf("close ==== %d %d\n", parameters[2 * i - 6] - '0', parameters[2 * i - 4] - '0');
+					i += 2;
+				}
+				i -= 2;
+				gen_segment(parameters[2 * i - 2] - '0', parameters[2 * i] - '0', parameters[2] - '0', parameters[4] - '0');
+				//printf("close ==== %d %d\n", parameters[2 * i - 2] - '0', parameters[2 * i] - '0');
+			}
+			else
+			{
+				printf("Invalid Parameter: Unvalidated Arguments!\n\n");
+				wizard(OPERATION, parameters);
+				continue;
+			}
+			canvas_print();
+			wizard(OPERATION, parameters);
+		}
+		else
+		{
+			printf("Invalid Input: Unselected Operation!\n\n");
+			wizard(OPERATION, parameters);
+		}
+	}
+	return 0;
+}
+
+int main()
+{
+	canvas_init();
+	wizard(OPERATION, parameters);
+	parameters_handle(parameters);
+	return 0;
+}
--- Course/graph_coordinate.c
+++ Course/graph_coordinate.c
@@ -10,7 +10,7 @@
 #include <string.h>
 
 #define COLUMN 68 // X-axis
-#define ROW 36    // Y-axis
+#define ROW 36	// Y-axis
 #define X_AXIS 34
 #define Y_AXIS 18
 
@@ -24,258 +24,260 @@ char parameters[MAX_PARAMETERS * 2] = {0};
 
 int canvas_init()
 {
-    for (int y = 0; y <= ROW; y++)
-    {
-        for (int x = 0; x < COLUMN; x++)
-        {
-            if ((y == Y_AXIS) || (x == X_AXIS))
-            {
-                canvas[y][x] = '.';
-            }
-            else
-            {
-                canvas[y][x] = ' ';
-            }
-        }
-    }
-    return 0;
+	for (int y = 0; y <= ROW; y++)
+	{
+		for (int x = 0; x < COLUMN; x++)
+		{
+			if ((y == Y_AXIS) || (x == X_AXIS))
+			{
+				canvas[y][x] = '.';
+			}
+			else
+			{
+				canvas[y][x] = ' ';
+			}
+		}
+	}
+	return 0;
 }
 
 int canvas_draw(int x, int y)
 {
-    if (canvas[x][y] == ' ')
-    {
-        canvas[x][y] = '.';
-    }
-    return 0;
+	if (canvas[x][y] == ' ')
+	{
+		canvas[x][y] = '.';
+	}
+	return 0;
 }
 
 int canvas_print()
 {
-    // Top border
-    printf(" +");
-    for (int i = 1; i < COLUMN; i++)
-    {
-        printf(" ─");
-    }
-    printf(" +");
-    printf("\n");
+	// Top border
+	printf(" +");
+	for (int i = 1; i < COLUMN; i++)
+	{
+		printf(" ─");
+	}
+	printf(" +");
+	printf("\n");
 
-    // Canvas
-    for (int i = 0; i <= ROW; i++) // Y-axis
-    {
-        for (int j = 0; j <= COLUMN * 2; j += 2) // X-axis
-        {
-            if (j == 0 || j == COLUMN * 2)
-            {
-                printf(" |");
-            }
-            else if ((i == Y_AXIS + 1) && (j == COLUMN + 2))
-            {
-                printf(" o");
-            }
-            else
-            {
-                printf(" %c", canvas[i][j / 2]);
-            }
-        }
-        printf("\n");
-    }
+	// Canvas
+	for (int i = 0; i <= ROW; i++) // Y-axis
+	{
+		for (int j = 0; j <= COLUMN * 2; j += 2) // X-axis
+		{
+			if (j == 0 || j == COLUMN * 2)
+			{
+				printf(" |");
+			}
+			else if ((i == Y_AXIS + 1) && (j == COLUMN + 2))
+			{
+				printf(" o");
+			}
+			else
+			{
+				printf(" %c", canvas[i][j / 2]);
+			}
+		}
+		printf("\n");
+	}
 
-    // Buttom border
-    printf(" +");
-    for (int i = 1; i < COLUMN; i++)
-    {
-        printf(" ─");
-    }
-    printf(" +");
-    printf("\n");
-    return 0;
+	// Buttom border
+	printf(" +");
+	for (int i = 1; i < COLUMN; i++)
+	{
+		printf(" ─");
+	}
+	printf(" +");
+	printf("\n");
+	return 0;
 }
 
 int wizard(int flag, char *args)
 {
-    if (flag == HELP)
-    {
-        printf("Example:\n");
-        printf("-------------- Operation ---------------\n");
-        printf("0) Exit\n");
-        printf("1) Help\n");
-        printf("2) Add\n");
-        printf("3) Clean\n");
-        printf("\n");
-        printf("Select an operation:\n");
-        printf("Graph>> a\n");
-        printf("0) Line\n");
-        printf("1) Coordinate\n");
-        printf("\n");
-        printf("Input type and parameters:\n");
-        printf("Graph>> l x1,y1 x2,y2 x3,y3 ...\n");
-        printf("---------------------------------------------------------------------\n");
-        printf("\n");
-        printf("Now select an operation:\n");
-        printf("Graph>> ");
-        scanf("%s", args);
-    }
-    else if (flag == OPERATION)
-    {
-        printf("\n");
-        printf("=============== Operation ==============\n");
-        printf("0) Exit\n");
-        printf("1) Help\n");
-        printf("2) Add\n");
-        printf("3) Clean\n");
-        printf("=====================================================================\n");
-        printf("\n");
-        printf("Select an operation:\n");
-        printf("Graph>> ");
-        scanf("%s", args);
-    }
-    else if (flag == ADD)
-    {
-        printf("=============== Arguments ==============\n");
-        printf("0) Line\n");
-        printf("1) Coordinate\n");
-        printf("=====================================================================\n");
-        printf("\n");
-        printf("Input type and parameters:\n");
-        printf("c 1,2 5,8 4,1\n");
-        printf("Graph>> ");
-        getchar();
-        fgets(args, MAX_PARAMETERS, stdin);
-    }
-    return 0;
+	if (flag == HELP)
+	{
+		printf("Example:\n");
+		printf("-------------- Operation ---------------\n");
+		printf("0) Exit\n");
+		printf("1) Help\n");
+		printf("2) Add\n");
+		printf("3) Clean\n");
+		printf("\n");
+		printf("Select an operation:\n");
+		printf("Graph>> a\n");
+		printf("0) Line\n");
+		printf("1) Coordinate\n");
+		printf("\n");
+		printf("Input type and parameters:\n");
+		printf("Graph>> l x1,y1 x2,y2 x3,y3 ...\n");
+		printf("---------------------------------------------------------------------\n");
+		printf("\n");
+		printf("Now select an operation:\n");
+		printf("Graph>> ");
+		scanf("%s", args);
+	}
+	else if (flag == OPERATION)
+	{
+		printf("\n");
+		printf("=============== Operation ==============\n");
+		printf("0) Exit\n");
+		printf("1) Help\n");
+		printf("2) Add\n");
+		printf("3) Clean\n");
+		printf("=====================================================================\n");
+		printf("\n");
+		printf("Select an operation:\n");
+		printf("Graph>> ");
+		scanf("%s", args);
+	}
+	else if (flag == ADD)
+	{
+		printf("=============== Arguments ==============\n");
+		printf("0) Line\n");
+		printf("1) Coordinate\n");
+		printf("=====================================================================\n");
+		printf("\n");
+		printf("Input type and parameters:\n");
+		printf("c 1,2 5,8 4,1\n");
+		printf("Graph>> ");
+		getchar();
+		fgets(args, MAX_PARAMETERS, stdin);
+	}
+	return 0;
 }
 
 int gen_line(int a, int b)
 {
-    for (int y = -Y_AXIS; y <= Y_AXIS; y++)
-    {
-        for (int x = -X_AXIS; x <= X_AXIS; x++)
-        {
-            if (y == a * x + b)
-            {
-                printf("----------\t%d = %d * %d + %d\n", y, a, x, b); // TODO:
-                canvas_draw(Y_AXIS - x, X_AXIS + y);
-                printf("==== %d %d\n", Y_AXIS - x, X_AXIS + y); //TODO:
-            }
-        }
-    }
-    return 0;
+	for (int y = -Y_AXIS; y <= Y_AXIS; y++)
+	{
+		for (int x = -X_AXIS; x <= X_AXIS; x++)
+		{
+			if (y == a * x + b)
+			{
+				// Can't Modified!!!
+				canvas_draw(Y_AXIS - y, X_AXIS + x);
+			}
+		}
+	}
+	return 0;
 }
 
 int gen_segment(int x_1, int y_1, int x_2, int y_2)
 {
-    int a = (y_1 - y_2) / (x_1 - x_2);
-    int b = (-x_1) * (y_1 - y_2) / (x_1 - x_2) + y_1;
-    // process the raw data
-    // example: x_1 < x_2
-    if (x_1 > x_2)
-    {
-        int temp = 0;
-        temp = x_2;
-        x_2 = x_1;
-        x_1 = temp;
-    }
+	// process the raw data
+	// aim: x_1 < x_2 and y_1<y_2
+	printf("before: %d %d %d %d\n", x_1, y_1, x_2, y_2); //TODO: cancel comment
+	if ((x_1 > x_2) || (y_1 > y_2))
+	{
+		int temp = 0;
+		// change x
+		temp = x_2;
+		x_2 = x_1;
+		x_1 = temp;
+		// change y
+		temp = y_2;
+		y_2 = y_1;
+		y_1 = temp;
+		printf("after: %d %d %d %d\n", x_1, y_1, x_2, y_2); //TODO: cancel comment
+	}
 
-    // a don't exist
-    if (x_1 == x_2)
-    {
-        // blow are for a don't exist
-        for (int y = y_1; y <= y_2; y++)
-        {
-            for (int x = x_1; x <= x_2; x++)
-            {
-                if (b == x)
-                {
-                    printf("%d %d\n", y, x); //TODO: cancel comment
-                    canvas_draw(Y_AXIS - y, X_AXIS + b);
-                }
-            }
-        }
-    }
-    else
-    {
-        //for (int y = -Y_AXIS; y <= Y_AXIS; y++)
-        for (int y = -x_1; y <= y_2; y++)
-        {
-            for (int x = -x_1; x <= x_2; x++)
-            {
-                if (y == a * x + b)
-                {
-                    //printf("%d %d\n", x, y); //TODO: cancel comment
-                    canvas_draw(Y_AXIS - x, X_AXIS + y);
-                }
-            }
-        }
-    }
-    return 0;
+	// a don't exist
+	if (x_1 == x_2)
+	{
+		// blow are for the condition that a don't exist
+		for (int y = y_1; y <= y_2; y++)
+		{
+			printf("y: %d x: %d\n", y, x_1); //TODO: cancel comment
+			canvas_draw(Y_AXIS - y, X_AXIS + x_1);
+		}
+	}
+	else
+	{
+		float a = (float)(y_2 - y_1) / (x_2 - x_1);
+		float b = (float)(-x_1) * (y_2 - y_1) / (x_2 - x_1) + y_1;
+		printf("a: %f b: %f\n", a, b); //TODO: cancel comment
+		for (int y = y_1; y <= y_2; y++)
+		{
+			for (int x = x_1; x <= x_2; x++)
+			{
+				if (y == a * x + b)
+				{
+					//printf("Enter\n"); //TODO: cancel comment
+					printf("y: %d x: %d\n", y, x); //TODO: cancel comment
+					//canvas_draw(Y_AXIS - y + 1, X_AXIS + x - 1);
+					canvas_draw(Y_AXIS - y, X_AXIS + x);
+				}
+			}
+		}
+	}
+	return 0;
 }
 
 // Core code
 int parameters_handle(char *parameters)
 {
-    while (strcmp(parameters, "exit") && strcmp(parameters, "quit") && strcmp(&parameters[0], "e") && strcmp(&parameters[0], "q"))
-    {
-        if (!strcmp(parameters, "help") || !strcmp(&parameters[0], "h"))
-        {
-            wizard(HELP, parameters);
-        }
-        else if (!strcmp(parameters, "clean") || !strcmp(&parameters[0], "c"))
-        {
-            canvas_init();
-            printf("Graph cleaned\n");
-            wizard(OPERATION, parameters);
-        }
-        else if (!strcmp(parameters, "add") || !strcmp(&parameters[0], "a"))
-        {
-            wizard(ADD, parameters);
-            if (parameters[0] == 'l') // line
-            {
-                if ((parameters[2] != '\0') && (parameters[4] != '\0'))
-                {
-                    //printf("%d %d\n", parameters[2] - '0', parameters[4] - '0');
-                    gen_line((parameters[2] - '0'), (parameters[4] - '0'));
-                }
-                else
-                {
-                    printf("Invalid Parameter: Missing Arguments!\n\n");
-                }
-            }
-            else if (parameters[0] == 'c') // coordinate
-            {
-                int i = 4;
-                while (parameters[2 * i] != '\0')
-                {
-                    gen_segment(parameters[2 * i - 6] - '0', parameters[2 * i - 4] - '0', parameters[2 * i - 2] - '0', parameters[2 * i] - '0');
-                    i += 2;
-                }
-                i -= 2;
-                gen_segment(parameters[2 * i - 2] - '0', parameters[2 * i] - '0', parameters[2] - '0', parameters[4] - '0');
-                printf("Jump\n");
-            }
-            else
-            {
-                printf("Invalid Parameter: Unvalidated Arguments!\n\n");
-                wizard(OPERATION, parameters);
-                continue;
-            }
-            canvas_print();
-            wizard(OPERATION, parameters);
-        }
-        else
-        {
-            printf("Invalid Input: Unselected Operation!\n\n");
-            wizard(OPERATION, parameters);
-        }
-    }
-    return 0;
+	while (strcmp(parameters, "exit") && strcmp(parameters, "quit") && strcmp(&parameters[0], "e") && strcmp(&parameters[0], "q"))
+	{
+		if (!strcmp(parameters, "help") || !strcmp(&parameters[0], "h"))
+		{
+			wizard(HELP, parameters);
+		}
+		else if (!strcmp(parameters, "clean") || !strcmp(&parameters[0], "c"))
+		{
+			canvas_init();
+			printf("Graph cleaned\n");
+			wizard(OPERATION, parameters);
+		}
+		else if (!strcmp(parameters, "add") || !strcmp(&parameters[0], "a"))
+		{
+			wizard(ADD, parameters);
+			if (parameters[0] == 'l') // line
+			{
+				if ((parameters[2] != '\0') && (parameters[4] != '\0'))
+				{
+					gen_line((parameters[2] - '0'), (parameters[4] - '0'));
+				}
+				else
+				{
+					printf("Invalid Parameter: Missing Arguments!\n\n");
+				}
+			}
+			else if (parameters[0] == 'c') // coordinate
+			{
+				int i = 4;
+				while (parameters[2 * i] != '\0')
+				{
+					gen_segment(parameters[2 * i - 6] - '0', parameters[2 * i - 4] - '0', parameters[2 * i - 2] - '0', parameters[2 * i] - '0');
+					//printf("close ==== %d %d\n", parameters[2 * i - 6] - '0', parameters[2 * i - 4] - '0');
+					i += 2;
+				}
+				i -= 2;
+				gen_segment(parameters[2 * i - 2] - '0', parameters[2 * i] - '0', parameters[2] - '0', parameters[4] - '0');
+				//printf("close ==== %d %d\n", parameters[2 * i - 2] - '0', parameters[2 * i] - '0');
+			}
+			else
+			{
+				printf("Invalid Parameter: Unvalidated Arguments!\n\n");
+				wizard(OPERATION, parameters);
+				continue;
+			}
+			canvas_print();
+			wizard(OPERATION, parameters);
+		}
+		else
+		{
+			printf("Invalid Input: Unselected Operation!\n\n");
+			wizard(OPERATION, parameters);
+		}
+	}
+	return 0;
 }
 
 int main()
 {
-    canvas_init();
-    wizard(OPERATION, parameters);
-    parameters_handle(parameters);
-    return 0;
+	canvas_init();
+	wizard(OPERATION, parameters);
+	parameters_handle(parameters);
+	return 0;
 }
